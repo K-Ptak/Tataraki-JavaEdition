729a29ed21ef7f6cb40f2186d8e7bf93cb74a680
Kamil <kamil2001p@gmail.com>
2023-01-27 14:04:17 +0100

    Add checking if 'slownik.txt' exists

--- src/Handlers/ErrorHandler.java
+++ src/Handlers/ErrorHandler.java
@@ -5,6 +5,7 @@ public class ErrorHandler {
         switch (id) {
             case 0 -> System.out.println("Nie podałeś litery!");
             case 1 -> System.out.println("Nie ma ani jednego slowa na podaną literę w słowniku");
+            case 2 -> System.out.println("Nie znaleziono pliku 'slownik.txt'");
         }
         System.exit(0);
     }
--- src/Handlers/FileReader.java
+++ src/Handlers/FileReader.java
@@ -6,9 +6,14 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class FileReader {
-    public static String[] loadDictionary(String filename) throws IOException {
-        List<String> listOfStrings;
-        listOfStrings = Files.readAllLines(Paths.get(filename));
+    public static String[] loadDictionary(String filename){
+        List<String> listOfStrings = null;
+        try {
+            listOfStrings = Files.readAllLines(Paths.get(filename));
+        } catch (IOException e) {
+            ErrorHandler.errorCodes(2);
+        }
+        assert listOfStrings != null;
         return listOfStrings.toArray(new String[0]);
     }
 }
