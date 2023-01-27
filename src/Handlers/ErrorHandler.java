34a8a8647b4cf590d274ad794082221f999ee100
Kamil <kamil2001p@gmail.com>
2023-01-27 12:47:51 +0100

    Correct misspelling

--- src/Handlers/ErrorHandler.java
+++ src/Handlers/ErrorHandler.java
@@ -4,7 +4,7 @@ public class ErrorHandler {
     public static void errorCodes(int id) {
         switch (id) {
             case 0 -> System.out.println("Nie podałeś litery!");
-            case 1 -> System.out.println("Nie ma ani jednego slowa na podaną literę w slowniku");
+            case 1 -> System.out.println("Nie ma ani jednego slowa na podaną literę w słowniku");
         }
         System.exit(0);
     }
